0155bcfb82e8307898467726b01997497b0f7257
Vinayak Kalaskar <vinayak1592@gmail.com>
2016-08-04 08:49:58 -0700

    Day 3: Conditional Statements

---
+++ ConditionsStatements.java
@@ -0,0 +1,43 @@
+/*
+ Task 
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class ConditionsStatements {
+   
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt(); 
+      scan.close();
+      String ans="";
+          
+      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+      if(n%2==1){
+         ans = "Weird";
+      }
+      // If n is even and in the inclusive range of 2 to 5
+      else if(n%2==0 && n<5 && n>1){
+          ans = "Not Weird";
+      }
+      // If n is even and in the inclusive range of 6 to 20
+      else if(n%2==0 && n<21 && n>5){
+          ans = "Weird";
+      }
+      // If n is even and greater than 20
+      else if(n%2==0 && n>20){
+          ans = "Not Weird";
+      }
+      System.out.println(ans);
+   }
+}
